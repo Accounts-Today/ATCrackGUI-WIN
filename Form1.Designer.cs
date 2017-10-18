d257cf1b95a961c2485ce07938612b975a03b99d
Nick Marley <nickmarley@my.smccd.edu>
2017-10-17 19:41:17 -0700

    Uploaded GUI files

---
+++ Form1.Designer.cs
@@ -0,0 +1,233 @@
+﻿namespace ATCrackGUI
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
+            this.checkedListBox1 = new System.Windows.Forms.CheckedListBox();
+            this.button1 = new System.Windows.Forms.Button();
+            this.openAccount = new System.Windows.Forms.OpenFileDialog();
+            this.openProxy = new System.Windows.Forms.OpenFileDialog();
+            this.button2 = new System.Windows.Forms.Button();
+            this.button3 = new System.Windows.Forms.Button();
+            this.button4 = new System.Windows.Forms.Button();
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.textBox2 = new System.Windows.Forms.TextBox();
+            this.textBox3 = new System.Windows.Forms.TextBox();
+            this.saveFinished = new System.Windows.Forms.SaveFileDialog();
+            this.label1 = new System.Windows.Forms.Label();
+            this.licenseBox = new System.Windows.Forms.TextBox();
+            this.textBox4 = new System.Windows.Forms.TextBox();
+            this.button5 = new System.Windows.Forms.Button();
+            this.saveUsernames = new System.Windows.Forms.SaveFileDialog();
+            this.SuspendLayout();
+            // 
+            // checkedListBox1
+            // 
+            this.checkedListBox1.FormattingEnabled = true;
+            this.checkedListBox1.Items.AddRange(new object[] {
+            "Scrape Usernames",
+            "Use Backconnect",
+            "Play sound on hit"});
+            this.checkedListBox1.Location = new System.Drawing.Point(12, 120);
+            this.checkedListBox1.Name = "checkedListBox1";
+            this.checkedListBox1.Size = new System.Drawing.Size(120, 94);
+            this.checkedListBox1.TabIndex = 0;
+            this.checkedListBox1.ItemCheck += new System.Windows.Forms.ItemCheckEventHandler(this.checkedListBox1_ItemCheck);
+            this.checkedListBox1.MouseUp += new System.Windows.Forms.MouseEventHandler(this.checkedListBox1_MouseUp);
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(12, 220);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(305, 49);
+            this.button1.TabIndex = 1;
+            this.button1.Text = "Start ATCrack";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // openAccount
+            // 
+            this.openAccount.FileName = "openAccount";
+            // 
+            // openProxy
+            // 
+            this.openProxy.FileName = "openProxy";
+            // 
+            // button2
+            // 
+            this.button2.Location = new System.Drawing.Point(138, 10);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(75, 23);
+            this.button2.TabIndex = 2;
+            this.button2.Text = "Open File...";
+            this.button2.UseVisualStyleBackColor = true;
+            this.button2.Click += new System.EventHandler(this.button2_Click);
+            // 
+            // button3
+            // 
+            this.button3.Location = new System.Drawing.Point(138, 36);
+            this.button3.Name = "button3";
+            this.button3.Size = new System.Drawing.Size(75, 23);
+            this.button3.TabIndex = 3;
+            this.button3.Text = "Open File...";
+            this.button3.UseVisualStyleBackColor = true;
+            this.button3.Click += new System.EventHandler(this.button3_Click);
+            // 
+            // button4
+            // 
+            this.button4.Location = new System.Drawing.Point(138, 61);
+            this.button4.Name = "button4";
+            this.button4.Size = new System.Drawing.Size(75, 23);
+            this.button4.TabIndex = 4;
+            this.button4.Text = "Save to...";
+            this.button4.UseVisualStyleBackColor = true;
+            this.button4.Click += new System.EventHandler(this.button4_Click);
+            // 
+            // textBox1
+            // 
+            this.textBox1.Location = new System.Drawing.Point(12, 64);
+            this.textBox1.Name = "textBox1";
+            this.textBox1.ReadOnly = true;
+            this.textBox1.Size = new System.Drawing.Size(120, 20);
+            this.textBox1.TabIndex = 5;
+            this.textBox1.Text = "Finished path";
+            // 
+            // textBox2
+            // 
+            this.textBox2.Location = new System.Drawing.Point(12, 36);
+            this.textBox2.Name = "textBox2";
+            this.textBox2.ReadOnly = true;
+            this.textBox2.Size = new System.Drawing.Size(120, 20);
+            this.textBox2.TabIndex = 6;
+            this.textBox2.Text = "Proxy List";
+            // 
+            // textBox3
+            // 
+            this.textBox3.Location = new System.Drawing.Point(12, 12);
+            this.textBox3.Name = "textBox3";
+            this.textBox3.ReadOnly = true;
+            this.textBox3.Size = new System.Drawing.Size(120, 20);
+            this.textBox3.TabIndex = 7;
+            this.textBox3.Text = "Account List";
+            // 
+            // saveFinished
+            // 
+            this.saveFinished.FileName = "saveFinished";
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.Location = new System.Drawing.Point(138, 120);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(84, 16);
+            this.label1.TabIndex = 8;
+            this.label1.Text = "License Key:";
+            // 
+            // licenseBox
+            // 
+            this.licenseBox.Location = new System.Drawing.Point(161, 139);
+            this.licenseBox.Name = "licenseBox";
+            this.licenseBox.Size = new System.Drawing.Size(156, 20);
+            this.licenseBox.TabIndex = 9;
+            // 
+            // textBox4
+            // 
+            this.textBox4.Enabled = false;
+            this.textBox4.Location = new System.Drawing.Point(12, 90);
+            this.textBox4.Name = "textBox4";
+            this.textBox4.ReadOnly = true;
+            this.textBox4.Size = new System.Drawing.Size(120, 20);
+            this.textBox4.TabIndex = 10;
+            this.textBox4.Text = "Username Path";
+            // 
+            // button5
+            // 
+            this.button5.Enabled = false;
+            this.button5.Location = new System.Drawing.Point(138, 88);
+            this.button5.Name = "button5";
+            this.button5.Size = new System.Drawing.Size(75, 23);
+            this.button5.TabIndex = 11;
+            this.button5.Text = "Save to...";
+            this.button5.UseVisualStyleBackColor = true;
+            this.button5.Click += new System.EventHandler(this.button5_Click);
+            // 
+            // saveUsernames
+            // 
+            this.saveUsernames.FileName = "saveUsernames";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(329, 281);
+            this.Controls.Add(this.button5);
+            this.Controls.Add(this.textBox4);
+            this.Controls.Add(this.licenseBox);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.textBox3);
+            this.Controls.Add(this.textBox2);
+            this.Controls.Add(this.textBox1);
+            this.Controls.Add(this.button4);
+            this.Controls.Add(this.button3);
+            this.Controls.Add(this.button2);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.checkedListBox1);
+            this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
+            this.MaximizeBox = false;
+            this.MinimizeBox = false;
+            this.Name = "Form1";
+            this.Text = "ATCrackGUI";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.CheckedListBox checkedListBox1;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.OpenFileDialog openAccount;
+        private System.Windows.Forms.OpenFileDialog openProxy;
+        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.Button button3;
+        private System.Windows.Forms.Button button4;
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.TextBox textBox2;
+        private System.Windows.Forms.TextBox textBox3;
+        private System.Windows.Forms.SaveFileDialog saveFinished;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.TextBox licenseBox;
+        private System.Windows.Forms.TextBox textBox4;
+        private System.Windows.Forms.Button button5;
+        private System.Windows.Forms.SaveFileDialog saveUsernames;
+    }
+}
+
---
+++ Form1.cs
@@ -0,0 +1,179 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Diagnostics;
+using System.Drawing;
+using System.IO;
+using System.Linq;
+using System.Runtime.InteropServices;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace ATCrackGUI
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void button3_Click(object sender, EventArgs e) // Proxy List
+        {
+            if (openProxy.ShowDialog() == System.Windows.Forms.DialogResult.OK)
+            {
+                textBox2.Text = System.IO.Path.GetFileName(openProxy.FileName);
+            }
+            else
+            {
+                MessageBox.Show("File not found!", "Oops!", MessageBoxButtons.OK, MessageBoxIcon.Error);
+            }
+        }
+
+        private void button2_Click(object sender, EventArgs e) // Account List
+        {
+            if (openAccount.ShowDialog() == System.Windows.Forms.DialogResult.OK)
+            {
+                textBox3.Text = System.IO.Path.GetFileName(openAccount.FileName);
+            }
+            else
+            {
+                MessageBox.Show("File not found!", "Oops!", MessageBoxButtons.OK, MessageBoxIcon.Error);
+            }
+        }
+
+        private void button4_Click(object sender, EventArgs e) // Save to - Finished
+        {
+            if (saveFinished.ShowDialog() == System.Windows.Forms.DialogResult.OK)
+            {
+                textBox1.Text = System.IO.Path.GetFileName(saveFinished.FileName);
+            }
+            else
+            {
+                MessageBox.Show("An error occured during file selection!", "Oops!", MessageBoxButtons.OK, MessageBoxIcon.Error);
+            }
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            ReadLicense();
+            checkDeps();
+        }
+
+        private void ReadLicense()
+        {
+            if (File.Exists(Environment.CurrentDirectory + "/license.txt") && File.ReadAllText(Environment.CurrentDirectory + "/license.txt").Length == 24)
+            {
+                licenseBox.Text = File.ReadAllText(Environment.CurrentDirectory + "/license.txt");
+            }else if (!File.Exists(Environment.CurrentDirectory + "/license.txt"))
+            {
+                File.Create(Environment.CurrentDirectory + "/license.txt");
+            }
+        }
+
+        private void checkDeps()
+        {
+            if (!File.Exists(Environment.CurrentDirectory + "/Cracker.jar"))
+            {
+                MessageBox.Show("Cracker.jar not found!\nMake sure they are in the same folder!", "Oops!", MessageBoxButtons.OK, MessageBoxIcon.Warning);
+                Environment.Exit(0);
+            }
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            if (licenseBox.Text.Length == 24 && File.Exists(openAccount.FileName) && File.Exists(openProxy.FileName) && saveFinished.CheckPathExists)
+            {
+                File.WriteAllText(Environment.CurrentDirectory + "/license.txt", licenseBox.Text);
+                string args = " -jar Cracker.jar -l " + licenseBox.Text + " -c " + openAccount.FileName + " -p " +
+                              openProxy.FileName + " -f " + saveFinished.FileName;
+                if (checkedListBox1.CheckedItems.Contains(checkedListBox1.Items[0]) && saveUsernames.FileName != "saveUsernames")
+                {
+                    args = args + " -u " + saveUsernames.FileName;
+                }
+                if (checkedListBox1.CheckedItems.Contains(checkedListBox1.Items[1]))
+                {
+                    args = args + " -bc";
+                }
+                if (checkedListBox1.CheckedItems.Contains(checkedListBox1.Items[2]))
+                {
+                    args = args + " -n";
+                }
+                else if(checkedListBox1.GetItemCheckState(0) == CheckState.Checked && saveUsernames.FileName != "saveUsernames")
+                {
+                    MessageBox.Show("Username save path not found!", "Oops!", MessageBoxButtons.OK, MessageBoxIcon.Error);
+                    return;
+                }
+
+                Process.Start("java", args);
+                Environment.Exit(0);
+
+            }
+            else if (licenseBox.Text.Length != 24)
+            {
+                MessageBox.Show("Invalid License!", "Oops!", MessageBoxButtons.OK, MessageBoxIcon.Error);
+            }
+            else
+            {
+                MessageBox.Show("Invalid File Path!", "Oops", MessageBoxButtons.OK, MessageBoxIcon.Error);
+            }
+        }
+
+        private bool IsRunning()
+        {
+            Process[] plist = Process.GetProcesses();
+            foreach (Process p in plist)
+            {
+                if (!String.IsNullOrEmpty(p.MainWindowTitle) && p.MainWindowTitle.Contains("Accounts Today Cracker"))
+                {
+                    DialogResult dr = MessageBox.Show("ATCrack is already running!\nWould you like me to close it?", "Oops!", MessageBoxButtons.YesNo,
+                        MessageBoxIcon.Exclamation);
+                    if (dr == DialogResult.Yes)
+                    {
+                        p.Kill();
+                        return true;
+                    }
+                    else if (dr == DialogResult.No)
+                    {
+                        return false;
+                    }
+                }
+            }
+            return false;
+        }
+
+        private void button5_Click(object sender, EventArgs e) // Save to - Usernames
+        {
+            if (saveUsernames.ShowDialog() == System.Windows.Forms.DialogResult.OK)
+            {
+                textBox4.Text = System.IO.Path.GetFileName(saveUsernames.FileName);
+            }
+            else
+            {
+                MessageBox.Show("An error occured during file selection!", "Oops!", MessageBoxButtons.OK, MessageBoxIcon.Error);
+            }
+        }
+
+        private void checkedListBox1_ItemCheck(object sender, ItemCheckEventArgs e)
+        {
+
+        }
+
+        private void checkedListBox1_MouseUp(object sender, MouseEventArgs e)
+        {
+            if (checkedListBox1.CheckedItems.Contains(checkedListBox1.Items[0]))
+            {
+                textBox4.Enabled = true;
+                button5.Enabled = true;
+            }
+            else if (!checkedListBox1.CheckedItems.Contains(checkedListBox1.Items[0]))
+            {
+                textBox4.Enabled = false;
+                button5.Enabled = false;
+            }
+        }
+    }
+}
